b7a8c214be12f7872ce98a8e6c3bf011ef554783
Robins George <georgerobins111@gmail.com>
2022-04-08 18:06:33 -0600

    sample file one

---
+++ src/main/java/Questions/ReverseEachWordString.java
@@ -0,0 +1,24 @@
+package Questions;
+
+public class ReverseEachWordString {
+
+	public static void main(String[] args) {
+
+		String str = "Welcome To Java";
+		String[] words = str.split(" ");
+		String reverseString = " ";
+
+		for (String w : words) {
+
+			String reverseWord = " ";
+			for (int i = w.length() - 1; 1 >= 0; i--) {
+				reverseWord = reverseWord + w.charAt(i);
+				reverseString = reverseString + reverseWord+ " ";
+			}
+
+		}
+
+		System.out.println(reverseString);
+	}
+
+}
---
+++ src/main/java/Questions/SimpleClasss.java
@@ -0,0 +1,10 @@
+package Questions;
+
+public class SimpleClasss{  
+	
+	public static void main(String[] args) {
+	
+	
+	}	
+
+} 
